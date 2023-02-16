9af87edffc9b68d8bcf86547b9edd7ffb990962b
Aka'aba Musa Akidi <sydeestack@gmail.com>
2023-02-16 15:27:34 -0800

    puts for printing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* This program demonstrates the use of the main function in C. */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
