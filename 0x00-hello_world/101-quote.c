708aea38eb5d41611daaa3385826f17a683d66bc
Aka'aba Musa Akidi <sydeestack@gmail.com>
2023-02-16 16:24:46 -0800

    added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,16 @@
 #include <unistd.h>
 
-int main(void) {
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+*/
+
+int main(void)
+{
 	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, msg, sizeof(msg) - 1);
-	return 1;
+
+	return (1);
 }
