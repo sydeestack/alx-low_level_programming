52caffb3970a10630aba168f40f88e3d5c5d601f
Aka'aba Musa Akidi <sydeestack@gmail.com>
2023-02-16 16:00:50 -0800

    another

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,17 @@
 
 int main(void)
 {
-	int i; 
-	char b; 
-	long num; 
-	long long num2; 
-	float amount; 
-	
-	printf("The size of a char: %d byte(s)\n", (unsigned)sizeof(b));
-        printf("The size of a int: %d byte(s)\n", (unsigned)sizeof(i));
-        printf("The size of a long: %d byte(s)\n", (unsigned)sizeof(num));
-        printf("The size of a long long: %d byte(s)\n", (unsigned)sizeof(num2));
-        printf("The size of a float: %d byte(s)\n", (unsigned)sizeof(amount));
+	int i;
+	char b;
+	long num;
+	long long num2;
+	float amount;
+
+	printf("The size of a char: %d byte(s)\n", (unsigned int)sizeof(b));
+	printf("The size of a int: %d byte(s)\n", (unsigned int)sizeof(i));
+	printf("The size of a long: %d byte(s)\n", (unsigned int)sizeof(num));
+	printf("The size of a long long: %d byte(s)\n", (unsigned int)sizeof(num2));
+	printf("The size of a float: %d byte(s)\n", (unsigned int)sizeof(amount));
+
 	return (0);
 }
