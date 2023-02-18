7d96d14fbd40b0980eb04d3ab5ea153cf020e1f2
Aka'aba Musa Akidi <sydeestack@gmail.com>
2023-02-18 14:49:21 -0800

    9 and the last mandatory one

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
