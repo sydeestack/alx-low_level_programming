cba9133cf902312ecac660f4fa3fa26ec494a7b4
Aka'aba Musa Akidi <sydeestack@gmail.com>
2023-03-02 14:16:07 -0800

    added

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+/**
+ * _strncpy - copy a string
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * @n: maximum number of characters to copy
+ *
+ * Return: pointer to destination string
+ */
+char *_strncpy(char *dest, const char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
