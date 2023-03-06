3649e481e7c301a1b08ed65ef0797423245278df
Aka'aba Musa Akidi <sydeestack@gmail.com>
2023-03-06 14:00:20 -0800

    added

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,13 +6,14 @@
  *
  * Return: Pointer to the destination memory area.
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-	    dest[i] = src[i];
+		dest[i] = src[i];
 	}
 
 	return (dest);
