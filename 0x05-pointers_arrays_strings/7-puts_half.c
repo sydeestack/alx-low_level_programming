43d0a4d377a66818ea61f787bfcfa8f05e54097e
Aka'aba Musa Akidi <sydeestack@gmail.com>
2023-03-06 15:32:43 -0800

    change the code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,46 +1,39 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- * example_function - brief description of what the function does
+ * puts_half - Prints the second half of a string, followed by a new line
  *
- * @param1: description of the first parameter
- * @param2: description of the second parameter
+ * @str: The input string
  *
- * Return: description of the return value (if any)
+ * Return: void
  */
-int example_function(int param1, char *param2)
+void puts_half(char *str)
 {
-	/* Declare variables with a single declaration per line */
-	int i, j, k;
+    int len = 0;
+    int i;
 
-	/* Use spaces to separate operators and after commas */
-	i = param1 + 2;
-	j = strlen(param2);
+    /* Calculate the length of the string */
+    while (str[len] != '\0')
+    {
+        len++;
+    }
 
-	/* Use a newline before a curly brace, and always use braces */
-	if (i > 10)
-	{
-		printf("i is greater than 10\n");
-	}
-	else
-	{
-		printf("i is less than or equal to 10\n");
-	}
+    /* Calculate the starting index for printing the second half of the string */
+    if (len % 2 == 0)
+    {
+        i = len / 2;
+    }
+    else
+    {
+        i = (len - 1) / 2;
+    }
 
-	/* Use a space after keywords like if, while, for, etc. */
-	for (k = 0; k < j; k++)
-	{
-		/* Use tabs for indentation */
-		if (param2[k] == ' ')
-		{
-			printf("Space found at position %d\n", k);
-		}
-	}
+    /* Print the second half of the string */
+    while (str[i] != '\0')
+    {
+        putchar(str[i]);
+        i++;
+    }
 
-	/* Use a blank line to separate logical blocks of code */
-	printf("\n");
-
-	/* Always include a return statement at the end of a function */
-	return (i * j);
+    putchar('\n');
 }
