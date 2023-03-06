18450ef94bb704d935e85d165bb0c509ab6236cd
Aka'aba Musa Akidi <sydeestack@gmail.com>
2023-03-06 15:05:16 -0800

    restructor

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,14 @@
 void print_rev(char *s)
 {
 	int length = 0;
+	int i;
 
 	/* Calculate length of the string */
 	while (*(s + length) != '\0')
 		length++;
 
 	/* Print the string in reverse */
-	for (int i = length - 1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 		putchar(*(s + i));
 
 	putchar('\n');
