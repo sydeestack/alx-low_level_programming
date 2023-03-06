6c1c357b4c106ed9dcd1aeebc92e62f966fc1c40
Aka'aba Musa Akidi <sydeestack@gmail.com>
2023-03-06 14:58:26 -0800

    added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+/**
+ * _puts - Prints a string to stdout followed by a new line
+ *
+ * @str: Pointer to a string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+
+	putchar('\n');
+}
